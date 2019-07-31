0a2b64a3c98f530d384fbcdb1be3655ab276affd
hqy36921 <53473640+hqy36921@users.noreply.github.com>
2019-07-31 10:13:43 +0800

    Add files via upload
    Add prescription

---
+++ client/client/pages/remoteDiagnosis/doctorData.js
@@ -0,0 +1,47 @@
+var doctorData = [
+  { id: 1, text: 'Internal', name:"Alice", title:"Doctor1", cls: "cur-doctor",contact:{
+    tel:"13000000000",
+    email:"aaa@xxx.com",
+    wechat:"sdfaad",
+    addr:"59 Xi Kang Road, Jing An District, 34323"
+  }},
+  {
+    id: 2, text: 'Surgical', name: "Bob", title: "Doctor2", cls: "", contact: {
+      tel: "13100000000",
+      email: "bbb@xxx.com",
+      wechat: "r3rr33",
+      addr: "628 Gao Ke Road, Pu Dong District, 203503"
+    }},
+  {
+    id: 3, text: 'Gynecology', name: "Claire", title: "Doctor3", cls: "", contact: {
+      tel: "13200000000",
+      email: "ccc@xxx.com",
+      wechat: "hadgte5",
+      addr: "123 Hua Xia Zhong Road, Pu Dong District, 342234"
+    }},
+  {
+    id: 4, text: 'Stomatology', name: "Maggie", title: "Doctor4", cls: "", contact: {
+      tel: "13300000000",
+      email: "ddd@xxx.com",
+      wechat: "r3rr33",
+      addr: "999 Chang Le Road, Xu Hui District, 310323"
+    }},
+  {
+    id: 5, text: 'Orthopedics', name: "Jack", title: "Doctor5", cls: "", contact: {
+      tel: "13400000000",
+      email: "eee@xxx.com",
+      wechat: "alkdhfae",
+      addr: "44 Nan Jing Xi Road, Huang Pu District, 210223"
+    } },
+  {
+    id: 6, text: 'Dentistry', name: "Ben", title: "Doctor6", cls: "", contact: {
+      tel: "13500000000",
+      email: "fff@xxx.com",
+      wechat: "adfasa",
+      addr: "145 Shang Dong Zhong Road, Huang Pu District, 34323"
+    } },
+]
+
+module.exports={
+  doctorData:doctorData
+}
